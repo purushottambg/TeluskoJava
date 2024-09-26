11cee3985576541a59d414862bd0aef5a34814e6
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-09-26 09:55:43 +0530

    first two java codes are ready to commit

---
+++ ReverseNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String [] args){
+        int rem=0;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number to reverse: ");
+        int num = scan.nextInt();
+        if (num<=1) {
+            rem=num%10;
+            num%=10;
+            System.out.print(rem);
+        }
+        System.out.print(num);
+        scan.close();
+    }
+}
