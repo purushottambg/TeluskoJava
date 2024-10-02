e00265338b9e80c75820df7ee2739ccfba904578
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-10-02 09:59:03 +0530

    Collection has been used in the program

--- LargestNumber.java
+++ LargestNumber.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class LargestNumber {
     public static void main(String[] arg){
-        int[] ar = new int[5];
+        ArrayList<Integer> arrayList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         for (int loop=0;loop<5;loop++){
             System.out.print("Enter array member: ");
-            ar[loop]=sc.nextInt();    
+            arrayList.add(sc.nextInt());
         }
-        int max=0;
-        for(int loop=0; loop<ar.length;loop++){
-            if(ar[loop]>max){
-                max=ar[loop];
-            }
-        }
-        System.out.println("Max number is: "+max);
+        System.out.println(("Max number is: "+Collections.max(arrayList)));
     }
 }
