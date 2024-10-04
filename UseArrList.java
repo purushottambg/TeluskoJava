4858625e9b6b3f259e87e6e4ba90da893ac16bd6
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-10-04 23:33:49 +0530

    Started studying Collection

---
+++ UseArrList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class UseArrList {
+    public static void main(String[] args) {
+        ArrayList<String> aStrings = new ArrayList<>();
+        aStrings.add("Purushottam");
+        System.out.println(aStrings);
+    }
+}
