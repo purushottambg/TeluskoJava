b7f6535ca8585e8cf664e58526cbc71243a1d875
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-10-07 20:10:39 +0530

    Array Sum

--- ArraySum.java
+++ ArraySum.java
@@ -3,8 +3,8 @@ public  class ArraySum {
         int[] numbers = {10, 20, 30, 40, 50};
         int sum = 0;
 
-        for (int number : numbers) {
-            sum += number;
+        for (int number : numbers) {    
+            sum += number;      //This is equivalent to sum=sum+number
         }
         System.out.println("Sum of array elements: " + sum);
     }
