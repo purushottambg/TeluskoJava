b9e004ec8c5450a9ee465a339e83753c536bc264
Purushottam Gutthe <purushottamgutthe6767@gmail.com>
2024-10-24 10:54:20 +0530

    Labeled break statement is completed

--- LabeledBreak.java
+++ LabeledBreak.java
@@ -1,14 +1,16 @@
 public class LabeledBreak {
     public static void main(String[] args) {
         
-        MailLoop:
-        for (int i=0;i<10;i++){
-            for(int j=0;j<i;j++){
+        int i,j;
+        MailLoop:     //Labeled Break Loop
+        for (i=0;i<10;i++){
+            for(j=0;j<=i;j++){
                 System.out.println("Inner: "+j+" Outer: "+i);
                 if (j==7 && i==7)
                     break MailLoop;
             }
-        }
-        System.out.println("I wanted to break at I=7 and J=7");
+            System.out.println( i + ( i==0 ? "th" :(i==1 ? "st" :(i==2? "nd" :( i==3 ? "rd" : "th" ))))+" Round is Complete\n");
+        }        
+        System.out.println("I wanted to break at I=7 and J=7 \n");
     }
 }
